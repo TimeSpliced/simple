2a54a74f4b04154baa086923dcb70fca3ebfd2eb
TimeSpliced <64799620+TimeSpliced@users.noreply.github.com>
2020-08-18 15:35:46 +0200

    Changed to func/hook api

--- template/src/index.js
+++ template/src/index.js
@@ -1,12 +1,19 @@
+//import { useEffect, useState } from 'preact/hooks';
 import './style';
-import { Component } from 'preact';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+function App() {
+
+  //const [x, setX] = useState(0);
+
+  /*useEffect(() => {
+    console.log("Effect !");
+  }, []);*/
+
+  return (
+    <>
+      <h1>Hello, World !</h1>
+    </>
+  );
 }
+
+export default App;
